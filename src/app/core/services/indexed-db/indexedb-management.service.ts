33b9896452f47a5759f4ce261dd6a669ed732a01
Saeid Mohammadi <saeid.mhd@gmail.com>
2024-07-21 15:46:09 +0330

    fix invoice discount

--- src/app/core/services/indexed-db/indexedb-management.service.ts
+++ src/app/core/services/indexed-db/indexedb-management.service.ts
@@ -8,7 +8,7 @@ import { UtilityService } from '../common/utility.service';
 export class IndexedDbManagementService {
 
   db: any;
-  private dbVersion = 1;
+  private dbVersion = 25;
   private dbName = 'MobileOrderingDb';
   StoreNameList: StoreNameModel[] = [];
 
