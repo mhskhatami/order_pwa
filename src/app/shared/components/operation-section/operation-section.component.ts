d9df5fd89106fa004e173c0e156a9e5f09afbb36
Mahsa <mhskhatami@gmail.com>
2024-07-24 10:12:24 +0330

    navigating to owns component

--- src/app/shared/components/operation-section/operation-section.component.ts
+++ src/app/shared/components/operation-section/operation-section.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-operation-section',
@@ -6,4 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './operation-section.component.css'
 })
 export class OperationSectionComponent {
+
+  constructor(private router: Router) { }
+
+  navigateToInvoice() {
+    this.router.navigate(['/invoice']);
+  }
+
+  navigateToPerson() {
+    this.router.navigate(['/people-list']);
+  }
+  navigateToReports() {
+    this.router.navigate(["/reports"]);
+  }
+
 }
